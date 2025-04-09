ab1493d7d04b124ffcdfad0360db6ead0271004b
SSSajid777 <sajidihsanabdullah@gmail.com>
2025-04-09 14:04:56 -0400

    Update Loops and Conditionals Event Planner Project.py

--- Loops and Conditionals Event Planner Project.py
+++ Loops and Conditionals Event Planner Project.py
@@ -3,4 +3,14 @@
 #Description: Making Event Planner which incorporates if statements, for and while loops.
 
 print("Welcome to the Event Planner!")
+ 
 
+#Get event type from user 
+#Using loop to 
+
+while True:
+    event_type=input("Enter the event type, options are casual or formal (case sensitive): ")
+    if event_type== "formal" or event_type=="casual":
+        break
+    print("Error: Please enter either formal or casual (case sensitive)")
+    
