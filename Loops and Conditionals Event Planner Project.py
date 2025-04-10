cbcb7385f1cf6016cb86707552a06125ec8445bb
SSSajid777 <sajidihsanabdullah@gmail.com>
2025-04-10 13:35:42 -0400

    Update Loops and Conditionals Event Planner Project.py

--- Loops and Conditionals Event Planner Project.py
+++ Loops and Conditionals Event Planner Project.py
@@ -32,6 +32,23 @@ print("Select the type of event from the list below:")
 if event_type=="casual" and event_location=="indoor":
     print("1. Birthday Party")
     print("2. Gender Reveal Party")
+    print("3. Halloween")
+    print("4. Easter")
+    print("5. Christmas")
+    print("6. Thanksgiving")
+    print("7. None of the above")
+    events=["Birthday Party","Gender Reveal Party", "Halloween", "Easter", "Christmas", "Thanksgiving","None of the above"]
+elif event_type=="casual" and event_location=="outdoor":
+    print("1. Outdoor Birthday Party")
+    print("2. Outdoor Gender Reveal Party")
+    print("3. Outdoor Barbeque")
+    print("4. Family/Friends Picnic")
+    print("5. None of the above")
+    events=["Outdoor Birthday Party", "Outdoor Gender Reveal Party", "Outdoor Barbeque", "Family/Friends Picnic", "None of the above"]
+
+    
+    
+
 
     
     
