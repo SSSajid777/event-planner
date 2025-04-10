6a67a19fbd8927acdf8511afb2a72ee20023034e
SSSajid777 <sajidihsanabdullah@gmail.com>
2025-04-10 14:18:38 -0400

    Update Loops and Conditionals Event Planner Project.py

--- Loops and Conditionals Event Planner Project.py
+++ Loops and Conditionals Event Planner Project.py
@@ -37,15 +37,41 @@ if event_type=="casual" and event_location=="indoor":
     print("5. Christmas")
     print("6. Thanksgiving")
     print("7. None of the above")
-    events=["Birthday Party","Gender Reveal Party", "Halloween", "Easter", "Christmas", "Thanksgiving","None of the above"]
+    events_list=["Birthday Party","Gender Reveal Party", "Halloween", "Easter", "Christmas", "Thanksgiving","None of the above"]
 elif event_type=="casual" and event_location=="outdoor":
     print("1. Outdoor Birthday Party")
     print("2. Outdoor Gender Reveal Party")
     print("3. Outdoor Barbeque")
     print("4. Family/Friends Picnic")
     print("5. None of the above")
-    events=["Outdoor Birthday Party", "Outdoor Gender Reveal Party", "Outdoor Barbeque", "Family/Friends Picnic", "None of the above"]
+    events_list=["Outdoor Birthday Party", "Outdoor Gender Reveal Party", "Outdoor Barbeque", "Family/Friends Picnic", "None of the above"]
+elif event_type=="formal" and event_location=="indoor":
+    print("1. Conferences")
+    print("2. Workshop")
+    print("3. Indoor Wedding")
+    print("4. None of the above")
+    events_list=["Conferences","Workshop", "Indoor Wedding", "None of the above"]
+elif event_type=="formal" and event_location=="outdoor":
+    print("1. Oudoor Wedding")
+    print("2. Company Picnic")
+    print("3. Charities/Fundraisers")
+    print("4. None of the above")
+    events_list=["Outdoor Wedding", "Company Picnic", "Charities/Fundraiser"]
 
+events_choice=0
+
+while True:
+    try:
+        events_choice=("Enter the number of the event to choose from 1-",str(len(events_list)))
+        if events_choice>=1 and events_choice<=len(events_list):
+            break
+        else:
+            print("Enter a valid number from 1-",(len(events_list)))
+    except ValueError as e:
+        print("Error", e)
+
+
+        
     
     
 
